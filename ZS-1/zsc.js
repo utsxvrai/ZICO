05b799ef480c8bcca0fc94902824fb04faf69cfb
Utsav Rai <utsavrai2115@gmail.com>
2024-03-03 06:40:56 +0530

    part 0.2

--- ZS-1/zsc.js
+++ ZS-1/zsc.js
@@ -37,3 +37,72 @@ console.log(a % b);
 console.log(a ** b);
 
 
+// Relational Operators
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log("\n");
+
+// Logical Operators
+
+console.log(a > b && a < c);
+console.log(a > b || a < c);
+console.log(!(a > b));
+
+console.log(10 && 6);
+
+// special numbers 
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+console.log(0/0);
+console.log(Infinity - Infinity);
+console.log(Infinity + Infinity);
+console.log(Infinity * Infinity);
+console.log(Infinity / Infinity);
+console.log(Infinity % Infinity);
+console.log(Infinity ** Infinity);
+console.log(Infinity == Infinity);
+console.log(Infinity != Infinity);
+console.log(Infinity > Infinity);
+console.log(Infinity < Infinity);
+console.log(Infinity >= Infinity);
+console.log(Infinity <= Infinity);
+
+
+
+// Bitwise Operators
+
+
+console.log(5 & 1);
+console.log(5 | 1);
+console.log(5 ^ 1);
+console.log("\n");
+
+// Abstract Equality Comparison (==) and Strict Equality Comparison (===)
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 === 5);
+console.log(5 == 5);
+console.log("\n");
+
+
+// Typeof Operator
+
+console.log(typeof 5);
+console.log(typeof "5");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function(){});
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof 0/0);
