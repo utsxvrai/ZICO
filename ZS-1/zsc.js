866fbe6d74c51ddaef5b6613d662c3730b99caad
Utsav Rai <utsavrai2115@gmail.com>
2024-03-03 05:35:30 +0530

    part 0.1

---
+++ ZS-1/zsc.js
@@ -0,0 +1,39 @@
+// learning variables
+var a = 5;
+let b = 10;
+const c = 15;
+
+console.log(a);
+console.log(a, b, c);   
+
+var marks = 100;
+var name = "ZS";
+var age;
+var company = null;
+console.log("Marks: ", marks);
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Company: ", company);
+
+var user = {
+    name: "ZS",
+    age: 25,
+    company: "ZS",
+    marks: 100
+};
+console.log (user);
+
+
+console.log("this is very annoying \n and i dont know what i am doing \t also i am not sure if i am doing it right \n")
+
+
+// Arithmetic Operators
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
