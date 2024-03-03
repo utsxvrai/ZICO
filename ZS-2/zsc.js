e723478de74dc67aa6bee841404d031c7b47369d
Utsav Rai <utsavrai2115@gmail.com>
2024-03-03 07:32:06 +0530

    part 0.3

---
+++ ZS-2/zsc.js
@@ -0,0 +1,43 @@
+// var n = 6;
+// if(n % 2 == 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+// console.log("\n");
+
+// console.log("Welcome to ZS-2");
+
+// var isPremium = true;
+// var isMember = true;
+
+// if(isMember){
+//     if(isPremium){
+//         console.log("You are a premium member");
+//     }
+//     else{
+//         console.log("You are a regular member");
+//     }
+// }
+// else{
+//     console.log("You are not a member");
+// }   
+
+// loops 
+
+// write a program to print 1 to 10 numbers
+
+let i =1;
+let sum = 0;
+
+while(i<=10){
+    sum+=i;
+    i++;
+}
+console.log(sum);
+
+
+for(let i = 1; i<=10; i++){
+    console.log(i);
+}   
