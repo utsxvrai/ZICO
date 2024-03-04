65465dd5ed14439d6d4022f0a4a8ea36512fc787
Utsav Rai <utsavrai2115@gmail.com>
2024-03-05 03:26:10 +0530

    part 0.4

---
+++ ZS-4/zsc.js
@@ -0,0 +1,15 @@
+let x= "abc"; //string literal
+let y= String("abc");
+let z= new String("abc"); //string object
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log(x==y);
+console.log(y==z);
+console.log(z==y);
+
+console.log(x===y);
+console.log(y===z);
+console.log(z===y);
